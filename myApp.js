2df2da59f32ae6820f455e403b52d442387d98bd
derek <derek.snow@gmail.com>
2014-07-30 23:20:08 -0500

    inital commit

---
+++ myApp.js
@@ -0,0 +1,31 @@
+angular.module('myApp', [])
+    .controller('FormCtrl', ['$scope',
+        function($scope) {
+            function subtotalCalc(basePrice, tax) {
+                return (basePrice + basePrice * tax / 100);
+            }
+
+            function tipCalc(subtotal, tipPercent) {
+                return (subtotal * tipPercent / 100);
+            }
+            $scope.data = {
+                subtotal: 0,
+                tip: 0,
+                total: 0,
+                tipTotal: 0,
+                mealCount: 0,
+                avgTipPerMeal: 0
+            };
+            $scope.submit = function() {
+                if ($scope.myForm.$valid) {
+                    console.log('The form is valid');
+                    $scope.data.mealCount++;
+                    $scope.data.subtotal = subtotalCalc($scope.basePrice, $scope.taxRate);
+                    $scope.data.tip = tipCalc($scope.data.subtotal, $scope.tipPercent);
+                    $scope.data.total = $scope.data.subtotal + $scope.data.tip;
+                    $scope.data.tipTotal = $scope.data.tip + $scope.data.tipTotal;
+                    $scope.data.avgTipPerMeal = $scope.data.tipTotal / $scope.data.mealCount;
+                }
+            };
+        }
+    ]);
