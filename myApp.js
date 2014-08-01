cae2aff4d1e017cb90ab4e7d874696b6b8f9ed51
derek <derek.snow@gmail.com>
2014-07-31 23:51:30 -0500

    added reset to reset the entire form to an initial state and cancel to clear the current user values

--- myApp.js
+++ myApp.js
@@ -8,6 +8,7 @@ angular.module('myApp', [])
             function tipCalc(subtotal, tipPercent) {
                 return (subtotal * tipPercent / 100);
             }
+
             $scope.data = {
                 subtotal: 0,
                 tip: 0,
@@ -16,6 +17,9 @@ angular.module('myApp', [])
                 mealCount: 0,
                 avgTipPerMeal: 0
             };
+	    // Preserve copy of original data for reset
+	    $scope.originalData = angular.copy($scope.data);
+
             $scope.submit = function() {
                 if ($scope.myForm.$valid) {
                     console.log('The form is valid');
@@ -25,7 +29,20 @@ angular.module('myApp', [])
                     $scope.data.total = $scope.data.subtotal + $scope.data.tip;
                     $scope.data.tipTotal = $scope.data.tip + $scope.data.tipTotal;
                     $scope.data.avgTipPerMeal = $scope.data.tipTotal / $scope.data.mealCount;
+                    $scope.submitted = false;
                 }
+            };
+
+            $scope.cancel = function() {
+                $scope.basePrice = "";
+                $scope.taxRate = "";
+                $scope.tipPercent = "";
+            };
+
+            $scope.reset = function() {
+                $scope.cancel();
+                $scope.data = angular.copy($scope.originalData);
+                $scope.myForm.$setPristine();
             };
         }
     ]);
